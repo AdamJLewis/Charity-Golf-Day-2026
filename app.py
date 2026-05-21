5d6a0aa78eb3c70a139dbc7b3dec7eaeb8da99e4
AdamJLewis <Adamjlewis1@hotmail.com>
2026-05-21 09:44:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-from webdriver_manager.chrome import ChromeDriverManager
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.binary_location = "/usr/bin/chromium"
+
+driver = webdriver.Chrome(
+    service=Service("/usr/bin/chromedriver"),
+    options=options
+)
 
 
 JUSTGIVING_URL = "https://www.justgiving.com/page/queens-head-charity-golf"
